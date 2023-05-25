1c9b4dd6e75238fe999fd6d037ca9445e1573d79
ManuelAchaval <manuachaval@hotmail.com>
2023-05-25 15:26:06 -0300

    modal parte 1

---
+++ main.js
@@ -0,0 +1,4 @@
+let cerrar =document.querySelectorAll(".close")[0];
+let abrir =document.querySelectorAll(".cta")[0];
+let modal =document.querySelectorAll(".modal")[0];
+let modalC =document.querySelectorAll(".modal-container")[0];
